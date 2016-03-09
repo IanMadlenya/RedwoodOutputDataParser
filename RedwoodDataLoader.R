06e357e50bf2697f2e863aeff4e7053e0350a84d
economicurtis <curtisesjunk@gmail.com>
2016-03-08 16:08:54 -0800

    function documentation

--- RedwoodDataLoader.R
+++ RedwoodDataLoader.R
@@ -3,21 +3,24 @@ require(dplyr)
 
 
 redwoodParser <- function(data, keys){
+  
+  # keep only desired keys
   data <- data %>% 
     dplyr::filter(Key %in% keys)
   
+  # convert json formatted column into "wide table" format
   KeyData <- data %>% select(Value) %>% mutate(Value = as.character(Value)) %>% tbl_df
   KeyData[1,] <- paste(c("[",KeyData[1,]), collapse = "")
   KeyData[nrow(KeyData),] <- paste(c(KeyData[nrow(KeyData),],"]"), collapse = "")
   KeyData <- paste(unlist(KeyData), collapse=",")
   KeyData <- fromJSON(KeyData)
-  
   KeyData <- lapply(KeyData, function(x) {
     x[sapply(x, is.null)] <- NA
     data.frame(x)
   })
   KeyData <- bind_rows(KeyData)
   
+  # recombine
   data <- data %>% select(-Value)
   data <- bind_cols(data, KeyData)
 }
