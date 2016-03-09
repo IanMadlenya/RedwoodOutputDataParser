f59dd3e109e34d78a86b708c41afaa2b1391e612
economicurtis <curtisesjunk@gmail.com>
2016-03-08 20:18:17 -0800

    parser code cleanup
    - resolve factor to char coersion warning
    - faster, shorter,

--- RedwoodDataLoader.R
+++ RedwoodDataLoader.R
@@ -11,14 +11,13 @@ redwoodParser <- function(data, keys){
   # convert json formatted column into "wide table" format
   #  first 5 rows just do subtle string manipulation....
   KeyData <- data %>% select(Value) %>% mutate(Value = as.character(Value)) %>% tbl_df
-  KeyData[1,] <- paste(c("[",KeyData[1,]), collapse = "")
-  KeyData[nrow(KeyData),] <- paste(c(KeyData[nrow(KeyData),],"]"), collapse = "")
   KeyData <- paste(unlist(KeyData), collapse=",")
+  KeyData <- paste(c("[", KeyData, "]"), collapse="")
   KeyData <- fromJSON(KeyData) #requires RJSONIO
 
   KeyData <- lapply(KeyData, function(x) {
     x[sapply(x, is.null)] <- NA
-    data.frame(x)
+    data.frame(x,stringsAsFactors=F)
   })
   KeyData <- bind_rows(KeyData) # requires dplyr
   
@@ -29,7 +28,7 @@ redwoodParser <- function(data, keys){
 
 # Example ==========
 # 
-# testData <- read.csv("ExperimentData.csv")
-# testData <- redwoodParser(data = testData,
-#               keys = c("trade", "cancel","offer_text"))
+testData <- read.csv("ExperimentData.csv")
+testData <- redwoodParser(data = testData,
+              keys = c("trade", "cancel","offer_text"))
 
