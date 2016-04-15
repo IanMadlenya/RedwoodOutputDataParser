7afe56ca6a6e721777a23b2d96c324482970d476
economicurtis <curtisesjunk@gmail.com>
2016-04-14 19:36:21 -0700

    update bubbles example

--- examples/example_bubbles.R
+++ examples/example_bubbles.R
@@ -1,36 +1,38 @@
 # Bubble example
 
 
+source("RedwoodDataLoader.R")
+
 # Bubbles -----
 
 Bub.testData <- read.csv("examples/data/Bubbles.csv")
+# Bub.testData <- read.csv("C:/Users/OKComputer/Downloads/Bubbles-2016-04-14 08-25-19.468240.csv")
 Bub.testData <- redwoodParser(data = Bub.testData,
                               keys = c("LOG_CONFIG","state"))
 
 
-#' apply config fields to all rows 
-Bub.testData.2 <- Bub.testData %>%
-  select(starts_with("LOG_CONFIG.")) %>%
+#' apply config fields to all rows
+
+
+Bub.testData.Config <- Bub.testData %>%
+  filter(Key =="LOG_CONFIG") %>%
+  select(1:5, starts_with("LOG_CONFIG.")) %>%
   mutate(
     LOG_CONFIG.groups = as.character(LOG_CONFIG.groups),
     LOG_CONFIG.groups = ifelse(LOG_CONFIG.groups == "NULL", NA, LOG_CONFIG.groups)
   )
-na.locf <- function(x){
-  x = unlist(x)
-  #x is a vector, replace any NA with the value that preceeds it. 
-  for (i in 2:length(x)){
-    if(is.na(x[i])){x[i] = x[i-1]}
-  }
-  as.vector(x)
-}
 
-for (Col in names(Bub.testData.2)){
-  Bub.testData.2[,Col] <- na.locf(Bub.testData.2[,Col])
-}
+Bub.testData.state <- Bub.testData %>%
+  filter(Key != "LOG_CONFIG") %>%
+  select(1:5, starts_with("state"))
 
-Bub.testData <- Bub.testData %>%
-  select(-starts_with("LOG_CONFIG.")) %>%
-  bind_cols(Bub.testData.2)
+
+Bub.testData <- left_join(
+  Bub.testData.state,
+  select(Bub.testData.Config, 
+         Period, starts_with("LOG_CONFIG."))
+)
+
 
 # -----
 #' #' state is a periodically generated variable (once every 110-120 milliseconds)
@@ -38,10 +40,6 @@ Bub.testData <- Bub.testData %>%
 #' #
 
 Bub.testData.2 <- Bub.testData %>%
-  
-  dplyr::filter(
-    Key == "state"
-  ) %>%
   mutate(
     temp.timeDist = Time %% 1000000000 / 1000000000,
     temp.time     = as.integer(Time / 1000000000)
@@ -59,6 +57,6 @@ Bub.testData.2 <- Bub.testData %>%
     temp.time.keep == 1
   ) %>%
   select(
-    -temp.timeDist:-temp.time.keep
+    -temp.timeDist,-temp.time.rank, -temp.time.keep
   )
 # write.csv(Bub.testData.2, file = "examples/data/Bub.testData.csv", row.names = F)
