f21d5635e35a55d522a6a4a67974e4b4cddfd0bf
Curtis Kephart <curtisesjunk@gmail.com>
2016-04-15 13:23:15 -0700

    Update example_bubbles.R

--- examples/example_bubbles.R
+++ examples/example_bubbles.R
@@ -1,43 +1,44 @@
 # Bubble example
 
-
+# Load dependencies and redwood parcer helper functions
 source("RedwoodDataLoader.R")
 
 # Bubbles -----
 
 Bub.testData <- read.csv("examples/data/Bubbles.csv")
-# Bub.testData <- read.csv("C:/Users/OKComputer/Downloads/Bubbles-2016-04-14 08-25-19.468240.csv")
+# Bub.testData <- read.csv("C:/Users/OKComputer/Downloads/Bubbles-2016-04-14 08-25-19.468240.csv") #from a local copy
 Bub.testData <- redwoodParser(data = Bub.testData,
-                              keys = c("LOG_CONFIG","state"))
+                              keys = c("LOG_CONFIG","state")) #only want two messages
 
-
-#' apply config fields to all rows
-
-
+# -----
+#' apply config fields to all rows ###################
 Bub.testData.Config <- Bub.testData %>%
   filter(Key =="LOG_CONFIG") %>%
   select(1:5, starts_with("LOG_CONFIG.")) %>%
   mutate(
-    LOG_CONFIG.groups = as.character(LOG_CONFIG.groups),
+    LOG_CONFIG.groups = as.character(LOG_CONFIG.groups),  #group col is an odd one, and requires a little extra work
     LOG_CONFIG.groups = ifelse(LOG_CONFIG.groups == "NULL", NA, LOG_CONFIG.groups)
   )
 
+# subject of data, only start variable
 Bub.testData.state <- Bub.testData %>%
   filter(Key != "LOG_CONFIG") %>%
   select(1:5, starts_with("state"))
 
-
+# merge back
 Bub.testData <- left_join(
   Bub.testData.state,
   select(Bub.testData.Config, 
          Period, starts_with("LOG_CONFIG."))
 )
-
+# mostly done
 
 # -----
-#' #' state is a periodically generated variable (once every 110-120 milliseconds)
-#' #' this keeps only one obs a second
-#' #
+# keeps only obs once a second #############
+# state is a periodically generated variable (once every 110-120 milliseconds)
+# that's too much data!
+# this keeps only one obs a second
+#' I know that there are about 8-10 obs a sec, this finds the ob most recently after a second-hand tick, and keeps only it. 
 
 Bub.testData.2 <- Bub.testData %>%
   mutate(
@@ -61,6 +62,7 @@ Bub.testData.2 <- Bub.testData %>%
   )
 # write.csv(Bub.testData.2, file = "examples/data/Bub.testData.csv", row.names = F)
 
+# ------
 # Plot Actions over time, within periods #################
 
 library(ggplot2)
