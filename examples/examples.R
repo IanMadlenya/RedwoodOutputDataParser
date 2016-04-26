40178235839cf6480aa0c0c90a24faad0e6e7ac3
economicurtis <curtisesjunk@gmail.com>
2016-04-26 11:20:39 -0700

    update examples, cuny matching experient

--- examples/examples.R
+++ examples/examples.R
@@ -16,11 +16,13 @@ BJP.testData <- redwoodParser(data = BJP.testData,
                           keys = c("state", "actions", "targets"))
 
 
-#  -----
+# Stephen O'Connell ------------
 
-matching.testData <- read.csv("examples/data/Matching_CUNYbaruch.CSV.csv")
+matching.testData <- read.csv("examples/data/Matching-Steve Active Dev -strategy method.csv")
 matching.testData <- redwoodParser(data = matching.testData,
-                                   keys = c("action", "action_owntype",
-                                            "action_partnertype", "exit",
-                                            "repeataction_owntype","repeataction_partnertype"))
-# write.csv(matching.testData, file = "examples/data/matching_testData.csv", row.names = F)
+                                   keys = c("action","action_round", "action_owntype",
+                                            "action_partnertype","action_partnerid", 
+                                            "exit", "repeataction_owntype",
+                                            "repeataction_partnertype","repeataction_partnerid",
+                                            "strategy"))
+
